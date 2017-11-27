b510fac7fe468696736fdf6f22689cd7b4526d92
Tim Richer <richert@wit.edu>
2017-11-27 01:13:45 -0500

    Moved main into getBox and created Game object and getGames and processGame. Need to fix issue with acronyms

--- test.py
+++ test.py
@@ -3,7 +3,75 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+class Game:
+    def __init__(self, winner, loser, link):
+        self.winner = winner
+        self.loser = loser
+        self.link = link
+
+def getGames(month, day, year):
+    url = "https://www.sports-reference.com/cbb/boxscores/index.cgi?month=" + month + "&day=" + day + "&year=" + year
+    page = requests.get(url)
+    scoreboard = BeautifulSoup(page.content, 'html.parser')
+    games = scoreboard.find_all('div', class_= 'game_summary nohover')
+    allObjects = []
+    for game in games:
+        winnerTag = game.find('tr', class_= 'winner').find_all('a')
+        loserTag = game.find('tr', class_= 'loser').find_all('a')
+        winner = winnerTag[0].get_text().replace(' ', '-').lower()
+        loser = loserTag[0].get_text().replace(' ', '-').lower()
+        for ch in ['\'', '(', ')']:
+            if ch in winner: winner = winner.replace(ch, '')
+            if ch in loser: winner = winner.replace(ch, '')
+        if len(winnerTag) > 1:
+            link = "https://www.sports-reference.com" + winnerTag[1]['href'].encode('utf-8')
+        else:
+            link = "https://www.sports-reference.com" + loserTag[1]['href'].encode('utf-8')
+        allObjects.append(
+            Game(
+                winner,
+                loser,
+                link
+            )
+        )
+    return allObjects
+    
+def getBox(html, team):
+    if team == 'umbc': team = 'maryland-baltimore-county'
+    if team == 'ucf': team = 'central-florida'
+    print 'box-score-basic-' + team
+    test = html.find('table', id='box-score-basic-' + team)
+    headers = test.select("thead th")
+    headers = headers[2:]
+    colHeads = []
+    for h in headers: colHeads.append(h.get_text())
+    colHeads = [h.encode("utf-8") for h in colHeads]
+    colHeads[0] = 'Name'
+    df = pd.DataFrame(columns=colHeads)   
+    rows = test.select("tbody tr")
+    rows.remove(rows[5])
+    for row in rows:
+        line = []
+        name = row.select("th")[0].get_text().encode('utf-8')
+        line.append(name)
+        data = row.select('td')
+        for x in data:
+            line.append(x.get_text().encode("utf-8"))
+        series = pd.Series(line,colHeads)
+        df = df.append([series], ignore_index=True)       
+    df.to_csv(team + ".csv")
+    
+def processGame(game):
+    page = requests.get(game.link)
+    html = BeautifulSoup(page.content, 'html.parser')
+    getBox(html, game.winner)
+    getBox(html, game.loser)
+
 def main():
+    games = getGames("11", "23", "2017")
+    for game in games:
+        processGame(game)
+    """
     page = requests.get("https://www.sports-reference.com/cbb/boxscores/2017-11-23-stanford.html")
     boxSoup = BeautifulSoup(page.content, 'html.parser')
     box = boxSoup.find_all('table', id='box-score-basic-florida')
@@ -31,6 +99,7 @@ def main():
         df = df.append([series], ignore_index=True)
         
     df.to_csv("test.csv")
+    """
         
 if __name__ == "__main__": main()
                 
