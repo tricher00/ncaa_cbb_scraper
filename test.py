917976da27519c53b26c6c0dd9f5cdce95338187
tricher00 <tricher00@gmail.com>
2017-11-29 00:35:32 -0500

    Fixed issue with nicknames as team names

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #import sys
 from bs4 import BeautifulSoup
 import requests
+import re
 import pandas as pd
 
 log = open("log.txt", "w")
@@ -22,11 +23,23 @@ def getGames(month, day, year):
     scoreboard = BeautifulSoup(page.content, 'html.parser')
     games = scoreboard.find_all('div', class_= 'game_summary nohover')
     allObjects = []
+    regex = "/cbb/schools/(.*)/"
     for game in games:
         winnerTag = game.find('tr', class_= 'winner').find_all('a')
         loserTag = game.find('tr', class_= 'loser').find_all('a')
-        winner = winnerTag[0].get_text().replace(' ', '-').lower()
-        loser = loserTag[0].get_text().replace(' ', '-').lower()
+        try:
+            winnerLink = winnerTag[0]['href'].encode('utf-8')
+            search = re.search(regex, winnerLink)
+            winner = search.group(1)
+        except:
+            winner = winnerTag[0].get_text().replace(' ', '-').lower()
+        try:
+            loserLink = loserTag[0]['href'].encode('utf-8')
+            search = re.search(regex, loserLink)
+            loser = search.group(1)
+        except:
+            loser = loserTag[0].get_text().replace(' ', '-').lower()
+
         for ch in ['\'', '(', ')']:
             if ch in winner: winner = winner.replace(ch, '')
             if ch in loser: winner = winner.replace(ch, '')
@@ -45,36 +58,37 @@ def getGames(month, day, year):
     
 def getBox(html, team):
     global log
-    halt = False
     
     if team in nicknames: team = nicknames[team]
+    
     print 'box-score-basic-' + team
     test = html.find('table', id='box-score-basic-' + team)
+    
     try:
         test.select("thead th")
     except AttributeError:
-        log.write("ERROR: Check " + team + '\n')
-        halt = True
-    if not halt:
-        headers = test.select("thead th")
-        headers = headers[2:]
-        colHeads = []
-        for h in headers: colHeads.append(h.get_text())
-        colHeads = [h.encode("utf-8") for h in colHeads]
-        colHeads[0] = 'Name'
-        df = pd.DataFrame(columns=colHeads)   
-        rows = test.select("tbody tr")
-        rows.remove(rows[5])
-        for row in rows:
-            line = []
-            name = row.select("th")[0].get_text().encode('utf-8')
-            line.append(name)
-            data = row.select('td')
-            for x in data:
-                line.append(x.get_text().encode("utf-8"))
-            series = pd.Series(line,colHeads)
-            df = df.append([series], ignore_index=True)       
-        df.to_csv(team + ".csv")
+        log.write("ERROR: " + team + " is an incorrect team name" + '\n')
+        return
+    
+    headers = test.select("thead th")
+    headers = headers[2:]
+    colHeads = []
+    for h in headers: colHeads.append(h.get_text())
+    colHeads = [h.encode("utf-8") for h in colHeads]
+    colHeads[0] = 'Name'
+    df = pd.DataFrame(columns=colHeads)   
+    rows = test.select("tbody tr")
+    rows.remove(rows[5])
+    for row in rows:
+        line = []
+        name = row.select("th")[0].get_text().encode('utf-8')
+        line.append(name)
+        data = row.select('td')
+        for x in data:
+            line.append(x.get_text().encode("utf-8"))
+        series = pd.Series(line,colHeads)
+        df = df.append([series], ignore_index=True)       
+    df.to_csv("csv/" + team + ".csv")
     
 def processGame(game):
     page = requests.get(game.link)
@@ -84,39 +98,10 @@ def processGame(game):
 
 def main():
     global log
-    games = getGames("11", "23", "2017")
+    games = getGames("11", "24", "2017")
     for game in games:
         processGame(game)
     log.close()
-    """
-    page = requests.get("https://www.sports-reference.com/cbb/boxscores/2017-11-23-stanford.html")
-    boxSoup = BeautifulSoup(page.content, 'html.parser')
-    box = boxSoup.find_all('table', id='box-score-basic-florida')
-    test = box[0]
-    
-    headers = test.select("thead th")
-    headers = headers[2:]
-    colHeads = []
-    for h in headers: colHeads.append(h.get_text())
-    colHeads = [h.encode("utf-8") for h in colHeads]
-    colHeads[0] = 'Name'
-    df = pd.DataFrame(columns=colHeads)
-    
-    rows = test.select("tbody tr")
-    rows.remove(rows[5])
-
-    for row in rows:
-        line = []
-        name = row.select("th")[0].get_text().encode('utf-8')
-        line.append(name)
-        data = row.select('td')
-        for x in data:
-            line.append(x.get_text().encode("utf-8"))
-        series = pd.Series(line,colHeads)
-        df = df.append([series], ignore_index=True)
-        
-    df.to_csv("test.csv")
-    """
         
 if __name__ == "__main__": main()
                 
